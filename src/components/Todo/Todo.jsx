206c7a9f384e4776dd8da1be13008c02dd1cc5f9
Seokho <seokho0120@gmail.com>
2023-08-29 21:17:38 +0900

    ✅  localStorage에 todo저장, input과 label 오류 수정(체크박스)

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -13,16 +13,23 @@ export default function Todo({ todo, onUpdate, onDelete }) {
 
   const handleDelete = () => onDelete(todo);
 
+  // input과 label을 연결할때 htmlFor와 id를 연결한다. 이때 id와 htmlFor에서 고유한 값을 사용하지 않으면 체크박스를 클릭할때 다른 체크박스가 선택되는 오류가 생김
+  // 결론은 고유한 값으로 id와 htmlFor를 설정해야함. -> 새로 만들어지는 todo의 id로 저장
   return (
     <li className={styles.todo}>
       <input
         className={styles.checkbox}
         type='checkbox'
-        id='checkbox'
+        id={todo.id}
+        // id='checkbox'
         checked={todo.status === 'completed'}
         onChange={handleChange}
       />
-      <label className={styles.text} htmlFor='checkbox'>
+      <label
+        className={styles.text}
+        // htmlFor='checkbox'
+        htmlFor={todo.id}
+      >
         {todo.text}
       </label>
       <span className={styles.icon}>
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,22 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 import styles from './TodoList.module.css';
 
 function TodoList({ filter }) {
-  const [todos, setTodos] = useState([
-    { id: '123', text: '장보기', status: 'active' },
-    { id: '124', text: '공부하기', status: 'active' },
-  ]);
+  const [todos, setTodos] = useState(() => readTodosFromLocalStorage());
+  // state를 함수로 넣어주면, 기억하고 있는 값이 있을 경우 다시 호출되지 않는다. 8.21 강의 다시 들어보기
 
-  const handleAdd = (todo) => setTodos([...todos, todo]);
+  // const [todos, setTodos] = useState([
+  //   { id: '123', text: '장보기', status: 'active' },
+  //   { id: '124', text: '공부하기', status: 'active' },
+  // ]);
+
+  // const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleAdd = (todo) => {
+    setTodos([...todos, todo]);
+
+    // const updateTodoList = [...todos, todo];
+    // localStorage.setItem('todos', JSON.stringify(updateTodoList));
+  };
 
   const handleUpdate = (updatedTodo) => {
     setTodos(todos.map((t) => (t.id === updatedTodo.id ? updatedTodo : t)));
+
+    // const updateTodoList = [...todos, updatedTodo];
+    // localStorage.setItem('todos', JSON.stringify(updateTodoList));
   };
 
-  const handleDelete = (deletedTodo) =>
+  const handleDelete = (deletedTodo) => {
     setTodos(todos.filter((t) => t.id !== deletedTodo.id));
+    // const updateTodoList = [...todos, deletedTodo];
+    // localStorage.setItem('todos', JSON.stringify(updateTodoList));
+  };
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  // useEffect(() => {
+  //   const storedTodos = localStorage.getItem('todos');
+  //   if (storedTodos) {
+  //     setTodos(JSON.parse(storedTodos));
+  //   }
+  // }, []);
 
   const filtered = getFilteredItems(todos, filter);
 
@@ -25,6 +51,13 @@ function TodoList({ filter }) {
       return todos;
     }
     return todos.filter((todo) => todo.status === filter);
+  }
+
+  function readTodosFromLocalStorage() {
+    console.log('~~~~~~~~~~');
+
+    const todos = localStorage.getItem('todos');
+    return todos ? JSON.parse(todos) : [];
   }
 
   return (
